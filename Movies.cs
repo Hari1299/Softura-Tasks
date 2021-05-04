3e1fbb5f213012bd902f3136634f1710fe60da8a
Hari1299 <75466867+Hari1299@users.noreply.github.com>
2021-05-04 17:38:06 +0530

    Add files via upload

---
+++ Movies.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CollectionsProject
+{
+    public class Movies : IComparable<Movies>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Duration { get; set; }
+
+        public int CompareTo([AllowNull] Movies other)
+        {
+            return this.Duration.CompareTo(other.Duration);
+        }
+        Dictionary<int, Movies> dictionary = new Dictionary<int, Movies>();
+        public void TakeMovieDetails()
+        {
+            Console.WriteLine("Please enter the movie name");
+            Name = Console.ReadLine();
+            double duration = 0;
+            Console.WriteLine("Please enter the movie duration");
+            while (!double.TryParse(Console.ReadLine(), out duration))
+            {
+                Console.WriteLine("Invalid entry for duration. Please try again");
+            }
+            Duration = duration;
+        }
+        public override string ToString()
+        {
+            return "Movie Id : " + Id + " Name : " + Name + " Duration : " + Duration;
+        }
+    }
+}
