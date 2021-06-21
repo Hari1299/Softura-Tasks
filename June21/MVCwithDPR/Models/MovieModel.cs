3c2a6c01259dbe2331949845bc154cb594d4d9b5
Hari1299 <75466867+Hari1299@users.noreply.github.com>
2021-06-21 15:57:46 +0530

    Add files via upload

---
+++ June21/MVCwithDPR/Models/MovieModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCusingDPR.Models
+{
+    public class MovieModel
+    {
+        public int Sno { get; set; }
+        public string Movie { get; set; }
+    }
+}
