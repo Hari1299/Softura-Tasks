d57fd01c9ffd35d616479bf819798d532796367c
Hari1299 <75466867+Hari1299@users.noreply.github.com>
2021-07-12 22:05:00 +0530

    Add files via upload

---
+++ EmployeeInfoTask/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeInformation.Models
+{
+    public class Employee
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public List<string> Skills { get; set; }
+    }
+}
